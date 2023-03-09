c04da3db1a26a0a246d38f891b53204b77070741
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-03-09 14:39:47 -0500

    commit tree 407462472f1dfa332c7f67e55455479bcff29505

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
 
+class CashRegister
+    attr_accessor  :discount, :total, :items, :purchased_item
+    # attr_reader 
+
+    def initialize(discount = 0)
+       @total = 0 
+       @discount = discount
+       @items = []
+    end
+
+    def add_item(title, price, qty = 1)
+        @purchased_item = (price * qty)
+        @total = (total + @purchased_item)
+        return qty.times {self.items.push(title)} 
+    end
+
+    def apply_discount
+       deduction= (self.total * (self.discount/100.0)).to_i
+        if self.discount != 0
+         @total =  self.total - deduction
+         return "After the discount, the total comes to $#{total}."
+        else
+        return "There is no discount to apply."
+      end
+    end
+    
+     def void_last_transaction
+     @total = self.total - self.purchased_item
+    end
+     
+
+
+
+
+
+end
+
